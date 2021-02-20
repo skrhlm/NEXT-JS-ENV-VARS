231796044c336df2995d8b9ef20f6c1141112113
Johan Skär Holm <johanskarholm@gmail.com>
2021-02-20 12:59:43 +0100

    Add getInitialProps in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,27 @@
+import App from 'next/app'
 import '../styles/globals.css'
 import Link from 'next/link'
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, ...rest }) {
   return (
   <>
   <nav>
     <Link href="/">static</Link>
     <Link href="/initial-props">getInitialProps</Link>
     <Link href="/server-side-props">serverSideProps</Link>
-    <Link href="/static-props">getStaticProps</Link>
+    <Link href="/static-props">geatStaticProps</Link>
   </nav>
   <main>
-  <Component {...pageProps} />
+  <Component {...pageProps} {...rest}/>
   </main>
   
   </>)
 }
 
 export default MyApp
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps, appGetInitialProps:true }
+}
